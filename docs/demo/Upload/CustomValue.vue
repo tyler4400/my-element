b308f988df538f98dbb8f100d98c13e1fdc629b8
tylerzzheng <tylerzzheng@tencent.com>
2025-01-15 02:11:43 +0800

    Upload 文档书写

--- docs/demo/Upload/CustomValue.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { ref } from 'vue'
-import Switch from '@/components/Switch/Switch.vue'
-const test = ref('right')
-</script>
-<template>
-  <Switch v-model="test" activeValue="right" inactiveValue="wrong"/>
-  <h4>model-value: {{test}}</h4>
-</template>
--- docs/demo/Upload/Text.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { ref } from 'vue'
-import Switch from '@/components/Switch/Switch.vue'
-const test = ref(false)
-</script>
-<template>
-  <Switch v-model="test" activeText="ON" inactiveText="OFF"/>
-</template>
